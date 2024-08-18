682c3fc905cd01ab04320e5cba2830af8a5328f1
caglarorhan <caglaror@gmail.com>
2024-08-18 13:23:19 -0500

    refactor: move all localStorage settings to the index (popup) page side

--- js/index.js
+++ js/index.js
@@ -1,19 +1,48 @@
-window.addEventListener('load', function() {
-    // Add a click event listener to the button
-    let languageOptions = {us:true, uk:true, au:true, ca:true, nz:true, za:true, ie:true, in:true, ph:true, sg:true};
-    document.querySelectorAll('.checkbox-container input[type="checkbox"]').forEach(ipaLanguageCheckBox =>{
-        ipaLanguageCheckBox.addEventListener('click', function() {
-            let statusOfIpaLanguage = ipaLanguageCheckBox.checked;
-            let ipaLanguage = ipaLanguageCheckBox.value;
-            languageOptions[ipaLanguage] = statusOfIpaLanguage;
-            localStorage.setItem('languageOptions', JSON.stringify(languageOptions));
+
+const phoneticPortal = {
+    languageOptions: {us:true, uk:true}, // {us:true, uk:true, au:false, ca:false, nz:false, za:false, ie:false, in:false, ph:false, sg:false}
+    init(){
+        this.createLanguageOptions();
+        this.languageSetting();
+        this.addCommonEvents();
+    },
+    createLanguageOptions(){
+        Object.keys(this.languageOptions).forEach(languageOption =>{
+            let newLanguageOptionCheckBoxLabel = document.createElement('label');
+            let newLanguageOptionCheckBox = document.createElement('input');
+            newLanguageOptionCheckBox.type = 'checkbox';
+            newLanguageOptionCheckBox.value = languageOption;
+            newLanguageOptionCheckBox.name = `ipa-${languageOption}`;
+            newLanguageOptionCheckBoxLabel.appendChild(newLanguageOptionCheckBox);
+            newLanguageOptionCheckBoxLabel.appendChild(document.createTextNode(languageOption.toUpperCase() + ' IPA'));
+            document.querySelector(`.checkbox-container`).appendChild(newLanguageOptionCheckBoxLabel);
         })
-    })
+    },
+    languageSetting(){
+        let languageOptions = localStorage.getItem('languageOptions');
+        if(languageOptions){
+            Object.keys(languageOptions).forEach(languageOption =>{
+                document.querySelector(`.checkbox-container input[value="${languageOption}"]`).checked = languageOptions[languageOption]; // HATA
+            })
+        }else{
+            this.languageOptions = JSON.parse(languageOptions);
+            document.querySelectorAll('.checkbox-container input[type="checkbox"]').forEach(ipaLanguageCheckBox =>{
+                ipaLanguageCheckBox.checked = this.languageOptions[ipaLanguageCheckBox.value];
+            })  
+        }
+    },
+    addCommonEvents(){
+        document.querySelectorAll('.checkbox-container input[type="checkbox"]').forEach(ipaLanguageCheckBox =>{
+            ipaLanguageCheckBox.addEventListener('click', ()=>{
+                let statusOfIpaLanguage = ipaLanguageCheckBox.checked;
+                let ipaLanguage = ipaLanguageCheckBox.value;
+                this.languageOptions[ipaLanguage] = statusOfIpaLanguage;
+                localStorage.setItem('languageOptions', JSON.stringify(this.languageOptions));
+            })
+        })
+    }
+}
 
-
-
-
-});
-
-
-
+window.addEventListener('load', function() {
+    phoneticPortal.init();
+})
